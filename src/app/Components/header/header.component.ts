326f6a58b232f92b431e3db6da4a4564b5468477
Shaan Sundar <shaan.idtindia@gmail.com>
2022-02-27 16:30:19 +0530

    v1.0.1 genki-first-iter

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -9,11 +9,16 @@ import { DataPipelineService } from 'src/app/Service/data-pipeline.service';
 })
 export class HeaderComponent implements OnInit {
   public Header:any;
+  public mobileToggle: boolean = false;
   constructor(public data: DataPipelineService) { 
     this.Header = data.data.Header
   }
 
   ngOnInit(): void {
+  }
+
+  toggleItems(){
+    this.mobileToggle = !this.mobileToggle;
   }
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'genki-orange': '#FF8E4F',
+        'genki-green-primary': '#028C8A',
+        'genki-green-secondary': '#42A3A1',
+        'genki-green-tertiary': '#336160',
+        'genki-red-primary': '#FD593D'
+      },
+    },
   },
   plugins: [],
 }
